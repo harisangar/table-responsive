3c2a3d4993e87c6083436fbadfa92b4c0691cbeb
harisangar <ganambihai10@gmail.com>
2022-09-09 08:18:26 +0530

    style change

--- src/components/Table.js
+++ src/components/Table.js
@@ -8,22 +8,22 @@ import {users} from './userData'
 function Table() {
   return (
  
-    <div className='m-2 rounded-lg'>
+    <div className='m-2 "'>
        
     
-   
-   <table className=" shadow-lg bg-white w-full mobile:w-full mobile:block">
-     <thead className=" mobile:hidden  ">
+   <div>
+   <table className="  shadow-lg rounded-lg bg-white w-full mobile:w-full mobile:block">
+     <thead className=" mobile:hidden">
      	<tr className=''>
-     	 <th className="bg-blue-100 border-none text-left px-8 py-4">ID</th>
-     	 <th className="bg-blue-100 border-none text-left px-8 py-4">Name</th>
-     	 <th className="bg-blue-100 border-none text-left px-8 py-4">Gender</th>
-     	 <th className="bg-blue-100 border-none text-left px-8 py-4">Age</th>
-     	 <th className="bg-blue-100 border-none text-left px-8 py-4">Mark1</th>
-         <th className="bg-blue-100 border-none text-left px-8 py-4">Mark2</th>
-     	 <th className="bg-blue-100 border-none text-left px-8 py-4">Status%</th>
-     	 <th className="bg-blue-100 border-none text-left px-8 py-4">City</th>
-         <th className="bg-blue-100 border-none text-left px-8 py-4">update</th>
+     	 <th className="bg-gray-100 border-none text-center px-8 py-4">ID</th>
+     	 <th className="bg-gray-100 border-none text-center px-8 py-4">Name</th>
+     	 <th className="bg-gray-100 border-none text-center px-8 py-4">Gender</th>
+     	 <th className="bg-gray-100 border-none text-center px-8 py-4">Age</th>
+     	 <th className="bg-gray-100 border-none text-center px-8 py-4">Mark1</th>
+         <th className="bg-gray-100 border-none text-center px-8 py-4">Mark2</th>
+     	 <th className="bg-gray-100 border-none text-center px-8 py-4">Status</th>
+     	 <th className="bg-gray-100 border-none text-center px-8 py-4">City</th>
+         <th className="bg-gray-100 border-none text-center px-8 py-4">update</th>
         </tr>
      </thead>
      <tbody className="mobile:w-full mobile:block">
@@ -31,35 +31,35 @@ function Table() {
         {
             users.map((user)=>{
                 return(
-                    <tr className="mobile:w-full mobile:block mobile:mb-1 even:bg-lime-200 odd:bg-lime-100 ">
-                    <td data-label="S.No" className="border-none px-8 py-4 mobile:w-full mobile:block mobile:text-left mobile:pl-[25%] mobile:relative
+                    <tr className="mobile:w-full mobile:block mobile:mb-1 even:bg-red-100 odd:bg-red-50 ">
+                    <td data-label="S.No" className="border-none pl-5 py-4 text-center mobile:w-full mobile:block mobile:text-center mobile:pl-[25%] mobile:relative
                   mobile:before:content-[attr(data-label)] mobile:before:absolute
-                  mobile:before:left-0 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">
+                  mobile:before:left-1 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">
                    {user.id}</td>
-                    <td data-label="Name" className="border-none px-8 py-4 mobile:w-full mobile:block mobile:text-left mobile:pl-[25%] mobile:relative
+                    <td data-label="Name" className="border-none pl-5 text-center py-4 mobile:w-full mobile:block mobile:text-center mobile:pl-[25%] mobile:relative
                   mobile:before:content-[attr(data-label)] mobile:before:absolute
-                  mobile:before:left-0 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Name}</td>
-                    <td data-label="Age" className="border-none px-8 py-4 mobile:w-full mobile:block mobile:text-left mobile:pl-[25%] mobile:relative
+                  mobile:before:left-1 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Name}</td>
+                    <td data-label="Age" className="border-none pl-5 text-center py-4 mobile:w-full mobile:block mobile:text-center mobile:pl-[25%] mobile:relative
                   mobile:before:content-[attr(data-label)] mobile:before:absolute
-                  mobile:before:left-0 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Gender}</td>
-                    <td data-label="Marks%" className="border-none px-8 py-4 mobile:w-full mobile:block mobile:text-left mobile:pl-[25%] mobile:relative
+                  mobile:before:left-1 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Gender}</td>
+                    <td data-label="Marks%" className="border-none pl-5 text-center py-4 mobile:w-full mobile:block mobile:text-center mobile:pl-[25%] mobile:relative
                   mobile:before:content-[attr(data-label)] mobile:before:absolute
-                  mobile:before:left-0 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Age}</td>
-                    <td data-label="Staus" className="border-none px-8 py-4 mobile:w-full mobile:block mobile:text-left mobile:pl-[25%] mobile:relative
+                  mobile:before:left-1 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Age}</td>
+                    <td data-label="Staus" className="border-none pl-5 text-center py-4 mobile:w-full mobile:block mobile:text-center mobile:pl-[25%] mobile:relative
                   mobile:before:content-[attr(data-label)] mobile:before:absolute
-                  mobile:before:left-0 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Mark1}</td>
-                 <td data-label="Age" className="border-none px-8 py-4 mobile:w-full mobile:block mobile:text-left mobile:pl-[25%] mobile:relative
+                  mobile:before:left-1 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Mark1}</td>
+                 <td data-label="Age" className="border-none pl-5 text-center py-4 mobile:w-full mobile:block mobile:text-center mobile:pl-[25%] mobile:relative
                   mobile:before:content-[attr(data-label)] mobile:before:absolute
-                  mobile:before:left-0 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Mark2}</td>
-                    <td data-label="Marks%" className="border-none px-8 py-4 mobile:w-full mobile:block mobile:text-left mobile:pl-[25%] mobile:relative
+                  mobile:before:left-1 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Mark2}</td>
+                    <td data-label="Marks%" className="border-none pl-5 text-center py-4 mobile:w-full mobile:block mobile:text-center mobile:pl-[25%] mobile:relative
                   mobile:before:content-[attr(data-label)] mobile:before:absolute
-                  mobile:before:left-0 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Status}%</td>
-                    <td data-label="Staus" className="border-none px-8 py-4 mobile:w-full mobile:block mobile:text-left mobile:pl-[25%] mobile:relative
+                  mobile:before:left-1 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Status}</td>
+                    <td data-label="Staus" className="border-none pl-0 py-4 text-center mobile:w-full mobile:block mobile:text-center mobile:pl-[25%] mobile:relative
                   mobile:before:content-[attr(data-label)] mobile:before:absolute
-                  mobile:before:left-0 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Status}</td>
-                   <td data-label="Staus" className="border-none px-8 py-4 mobile:w-full mobile:block mobile:text-left mobile:pl-[25%] mobile:relative
+                  mobile:before:left-1 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{user.Status}</td>
+                   <td data-label="Staus" className="border-none pl-0 py-4 mobile:w-full mobile:block mobile:text-center mobile:pl-[25%] mobile:relative
                   mobile:before:content-[attr(data-label)] mobile:before:absolute
-                  mobile:before:left-0 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{<Button/>}</td>
+                  mobile:before:left-1 mobile:before:w-[25%]  mobile:before:text-mobile mobile:before:font-bold mobile:begore:text-left">{<Button/>}</td>
               </tr> 
 
                 );
@@ -68,6 +68,7 @@ function Table() {
         }
         </tbody>
    </table>
+   </div>
    </div>
     
   )   
--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -3,11 +3,11 @@ import React from 'react';
 function Button() {
 
   return (
-    <>
-    <button className="btn btn-danger mr-5 " >
+    <div>
+    <button className="btn btn-danger mr-5 transform  transition duration-500 hover:scale-125 ease-in-out" >
     <i className="icon-trash icon-large"></i> Delete</button>
-    <button className="btn btn-primary" ><i class="icon-edit-sign icon-large"></i> Edit</button>
-    </>
+    <button className="btn btn-primary transform  transition duration-500 hover:scale-125 ease-in-out" ><i class="icon-edit-sign icon-large"></i> Edit</button>
+    </div>
   )
 }
 
